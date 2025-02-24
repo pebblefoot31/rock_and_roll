1ef7e078b23523eaf727310759635cec63f9847b
nai08747 <nia.maheshwari@kub.org>
2025-02-24 15:02:31 -0500

    Pg63: sets up basic routing and hbs for a master-detail structure of songs for different bands.

---
+++ app/routes/bands.js
@@ -0,0 +1,70 @@
+import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
+
+//class for creating new band
+class Band {
+  @tracked name;
+
+  constructor({ id, name, songs }) {
+    this.id = id;
+    this.name = name;
+    this.songs = songs;
+  }
+}
+
+//class for creating new song
+class Song {
+  constructor({ title, rating, band }) {
+    this.title = title;
+    this.rating = rating ?? 0; //default to 0 stars if no rating is available
+    this.band = band;
+  }
+}
+
+export default class BandsRoute extends Route {
+  model() {
+    let blackDog = new Song({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3,
+    });
+
+    let yellowLedbetter = new Song({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4,
+    });
+
+    let pretender = new Song({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2,
+    });
+
+    let daughter = new Song({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5,
+    });
+
+    let ledZeppelin = new Band({
+      id: 'led-zeppelin',
+      name: 'Led Zeppelin',
+      songs: [blackDog],
+    });
+
+    let pearlJam = new Band({
+      id: 'pearl-jam',
+      name: 'Pearl Jam',
+      songs: [yellowLedbetter, daughter],
+    });
+
+    let fooFighters = new Band({
+      id: 'foo-fighters',
+      name: 'Foo Fighters',
+      songs: [pretender],
+    });
+
+    return [ledZeppelin, pearlJam, fooFighters];
+  }
+}
