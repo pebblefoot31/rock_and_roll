14ef3c4d82da91494449d61037a723f78749ed0c
nai08747 <nia.maheshwari@kub.org>
2025-03-03 12:29:00 -0500

    feat (Pg136): enable updating of ratings for songs loaded from backend.

--- app/models/song.js
+++ app/models/song.js
@@ -1,8 +1,13 @@
+import { tracked } from '@glimmer/tracking';
 //redefining domain models in our models directory instead of in the route
 export default class Song {
-  constructor({ title, rating, band }) {
+  @tracked rating;
+
+  constructor({ id, title, rating, band }, relationships = {}) {
+    this.id = id;
     this.title = title;
     this.rating = rating ?? 0;
     this.band = band;
+    this.relationships = relationships;
   }
 }
