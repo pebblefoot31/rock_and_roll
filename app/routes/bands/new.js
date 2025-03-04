4e61b0969cac590ac9afe92b8493a6c97ed6d394
nai08747 <nia.maheshwari@kub.org>
2025-03-04 09:28:32 -0500

    feat (pg160): refactors code to have backend plumbing primarily in services/catalog.js.

--- app/routes/bands/new.js
+++ app/routes/bands/new.js
@@ -1,5 +1,10 @@
 import Route from '@ember/routing/route';
 
+/*this ensures that the form for entering a new band is cleared once
+ * the user navigates away from the page. It does so by setting the
+ * name property on a controller to an empty string ''.
+ */
+
 export default class BandsNewRoute extends Route {
   resetController(controller) {
     controller.name = '';
--- app/services/catalog.js
+++ app/services/catalog.js
@@ -1,6 +1,21 @@
 import Service from '@ember/service';
+import Band from 'rarwe/models/band';
+import Song from 'rarwe/models/song';
 import { tracked } from 'tracked-built-ins';
+import { isArray } from '@ember/array';
 
+/* The catalog handles all back-end operations as it serves as a storage
+ * for the models, bands, and songs. The pieces of our app closer to the UI
+ * no longer have any low-level network plumbing code as a result of our
+ * refactoring! */
+
+function extractRelationships(object) {
+  let relationships = {};
+  for (let relationshipName in object) {
+    relationships[relationshipName] = object[relationshipName].links.related;
+  }
+  return relationships;
+}
 export default class CatalogService extends Service {
   storage = {};
 
@@ -10,9 +25,121 @@ export default class CatalogService extends Service {
     this.storage.songs = tracked([]);
   }
 
+  async fetchAll(type) {
+    if (type == 'bands') {
+      let response = await fetch('/bands');
+      let json = await response.json();
+      this.loadAll(json);
+      return this.bands;
+    }
+
+    if (type == 'songs') {
+      let response = await fetch('/songs');
+      let json = await response.json();
+      this.loadAll(json);
+      return this.songs;
+    }
+  }
+
+  loadAll(json) {
+    let records = [];
+    for (let item of json.data) {
+      records.push(this._loadResource(item));
+    }
+    return records;
+  }
+
+  /* Takes care of all the details of creating a new model record and
+   * adding it to the catalog so fetchAll can be kept rather tidy.
+   * The method takes a single parameter, the type of resources
+      to be fetched*/
+
+  load(response) {
+    return this._loadResource(response.data);
+  }
+
+  _loadResource(data) {
+    let record;
+    let { id, type, attributes, relationships } = data;
+    if (type == 'bands') {
+      let rels = extractRelationships(relationships);
+      record = new Band({ id, ...attributes }, rels);
+      this.add('band', record);
+    }
+    if (type == 'songs') {
+      let rels = extractRelationships(relationships);
+      record = new Song({ id, ...attributes }, rels);
+      this.add('song', record);
+    }
+    return record;
+  }
+
+  /* This method requests the back-end for related record and loads them
+   *  to the storage of the catalog. It also assigns the created, related
+   * records to a 'relationship' property like the songs property of Band
+   * model instances.
+   */
+
+  async fetchRelated(record, relationship) {
+    let url = record.relationships[relationship];
+    let response = await fetch(url);
+    let json = await response.json();
+    if (isArray(json.data)) {
+      record[relationship] = this.loadAll(json);
+    } else {
+      record[relationship] = this.load(json);
+    }
+    return record[relationship];
+  }
+
+  async create(type, attributes, relationships = {}) {
+    let payload = {
+      data: {
+        type: type === 'band' ? 'bands' : 'songs',
+        attributes,
+        relationships,
+      },
+    };
+    let response = await fetch(type === 'band' ? '/bands' : '/songs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify(payload),
+    });
+    let json = await response.json();
+    return this.load(json);
+  }
+
+  async update(type, record, attributes) {
+    let payload = {
+      data: {
+        id: record.id,
+        type: type === 'band' ? 'bands' : 'songs',
+        attributes,
+      },
+    };
+    let url = type === 'band' ? '/bands/${record.id}' : '/songs/${record.id}';
+    await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify(payload),
+    });
+  }
+
+  /* This change makes our collections 'identity maps' because items in the
+   * collection are unique on their ids. Even though the collections used in
+   * this app are arrays and not maps, the idea is the same. Eliminates bugs
+   * from having multiple identical records in the record! */
+
   add(type, record) {
     let collection = type === 'band' ? this.storage.bands : this.storage.songs;
-    collection.push(record);
+    let recordIds = collection.map((record) => record.id);
+    if (!recordIds.includes(record.id)) {
+      collection.push(record);
+    }
   }
 
   get bands() {
